064ccc16ba251b5f463b421d1593e202ee15c188
zach0987654321 <tracywolf05@outlook.com>
2023-12-11 14:41:52 -0600

    define stugg
    ewqrqwerqwer

---
+++ py.py
@@ -0,0 +1,33 @@
+#start of project 2
+
+def contacts_main():
+    #accepts no arguments
+    #drives the program
+    
+    
+def contacts_menu():
+    #sdfksdhfksjdhfkhfsdfdkhf
+    
+    
+def contacts_add():
+    #fwejoierwji
+    
+    
+def contacts_search():
+    #ji
+    
+    
+def contacts_edit():
+    #uhkjhsdkfjhdskjdsfh
+    
+    
+def contacts_delete():
+    #r
+    
+    
+def contacts_display():
+    #POOP FART
+    
+    
+
+    
