753a7e91dee024215c24cb92346eddfa59c54d12
zach0987654321 <tracywolf05@outlook.com>
2023-12-12 14:04:08 -0600

    Your

--- py.py
+++ py.py
@@ -3,7 +3,27 @@
 def contacts_main():
     #accepts no arguments
     #drives the program
+    #calls contacts_menu and gets the choice from it
     
+    #prime the loop
+    choice = int(contacts_menu())
+    
+    #validate menu choice
+   
+        
+    #loop to call the desired function
+    while choice != 5:
+        if choice == 1:
+                contacts_add()
+        elif choice == 2:
+                contacts_edit()
+        elif choice == 3:
+                contacts_delete()
+        elif choice == 4:
+              contacts_display()
+                
+    choice = int(contacts_menu())
+    print('Thank you for using the program. However, your mother.')
     
 def contacts_menu():
     #sdfksdhfksjdhfkhfsdfdkhf
